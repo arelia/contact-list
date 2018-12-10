355fb4c28d12d74e99e40478c199f5d2c3aedff8
arelia <arelia.jones@gmail.com>
2018-12-10 05:48:40 -0600

    add contacts service

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {getContacts} from './services/contacts';
 
-var data = JSON.stringify(false);
-
-var xhr = new XMLHttpRequest();
-
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === this.DONE) {
-    console.log(this.responseText);
-  }
-});
-
-xhr.open("GET", "https://shielded-taiga-83845.herokuapp.com/https://lamppoststudios.activehosted.com/api/3/contacts");
-xhr.setRequestHeader("api-token", [API_TOKEN]);
-
-xhr.send(data);
+let text = getContacts.then(function (value) {
+  return value;
+})
 
 class App extends Component {
   render() {
+
+
     return (
       <div className="App">
         <header className="App-header">
